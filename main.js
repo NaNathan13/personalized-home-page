66d33c58e4350ab89d5307bc19626b4e84a1734a
NaNathan13 <natewilson135@gmail.com>
2019-08-25 13:13:57 -0400

    removed a lot of styles but got most functionality working and background working. Need to add ability to edit goals

--- main.js
+++ main.js
@@ -19,9 +19,14 @@ let time = document.querySelector('#time'),
 
 function showTime(){
     let today = new Date(),
+        day = today.getDate(),
+        month = today.getMonth() + 1,
+        year = today.getFullYear(),
         hour = today.getHours(),
         min = today.getMinutes(),
         sec = today.getSeconds()
+//Date
+    todayDate.innerHTML = month + '/' + day + '/' + year;
 
 //AM PM
 const amPM = hour >= 12 ? 'AM' : 'PM';
@@ -39,32 +44,13 @@ function addZero(num){
  return (parseInt(num, 10) < 10 ? '0' : '') + num;
 }
 
-//set background and greeting
-function setBackground(){
-    let today = new Date(),
-     hour = today.getHours();
-
-     if(hour > 23 && hour < 4){
-        document.body.style.backgroundImage = "url('img/polygon1')";
-     } else if(hour >= 5 && hour < 9){
-        document.body.style.backgroundImage = "url('img/polygon2')";
-     } else if(hour >= 9 && hour < 13){
-        document.body.style.backgroundImage = "url('img/polygin3')";
-     } else if(hour >= 13 && hour < 17){
-        document.body.style.backgroundImage = "url('img/triangle1')";
-     } else if(hour >= 17 && hour < 20){
-        document.body.style.backgroundImage = "url('img/triangle2')";
-     } else if(hour == 20 || hour == 21 || hour == 22 || hour == 23 ){
-        document.body.style.backgroundImage = "url('img/polygon4')";
-     }
-}
-
 function setGreeting(){
     let today = new Date(),
      hour = today.getHours();
     if(hour < 12){
         //morning
         greeting.textContent="Good Morning";
+
     } else if(hour < 18 && hour > 11){
         //afternoon
         greeting.textContent="Good Afternoon";
@@ -76,11 +62,34 @@ function setGreeting(){
 
 let dayInMilliseconds = 1000 * 60 * 60 * 24;
 
+//Wedding countdown
+var countDownDate = new Date("May 16, 2020 11:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+  // Display the result in the element with id="demo"
+  daysTilWedding.innerHTML = days + 1;
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    daysTilWedding.innerHTML = "TODAY IS THE DAYI GET TO MARRY THE LOVE OF MY LIFE";
+  }
+}, 1000);
 
 
 //run functions 
 showTime();
-setBackground();
 setGreeting();
 setInterval(function() { 
     let daysLeft = 279
